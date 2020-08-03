a2b625dbde75b061ab601c2ed2e7f959c26fdb61
mohamedalmograby <mohamedalmograby@gmail.com>
2020-08-03 06:02:16 -0700

    'me'

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
+Route::post('/upload-file', 'ProductController@uploadFile');
+
+Route::get('/products', 'ProductController@index');
+Route::get('/products/{product}', 'ProductController@show');
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('/users' , 'UserController@index');
+    Route::get('/users/{user}/orders','UserController@showOrders');
+    Route::patch('/orders/{order}/deliver' , 'OrderController@deliverOrder');
+    Route::resource('/products', 'ProductController')->except(['index','show']);
+    Route::resource('/orders' , 'OrderController');
+});
--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/api/login', 'UserController@login');
-Route::post('/api/register', 'UserController@register');
-Route::post('/api/upload-file', 'ProductController@uploadFile');
-
-Route::get('/api//products', 'ProductController@index');
-Route::get('/api//products/{product}', 'ProductController@show');
-
-Route::group(['middleware' => 'auth:api'], function(){
-    Route::get('/api//users' , 'UserController@index');
-    Route::get('/api/users/{user}/orders','UserController@showOrders');
-    Route::patch('/api/orders/{order}/deliver' , 'OrderController@deliverOrder');
-    Route::resource('/api/products', 'ProductController')->except(['index','show']);
-    Route::resource('/api/orders' , 'OrderController');
-});
-
-
 Route::get('/lol' , function(){
     return 'lol' ; 
 }) ; 
