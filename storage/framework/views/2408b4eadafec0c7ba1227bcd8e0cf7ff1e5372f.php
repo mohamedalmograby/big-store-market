1d2ebcf0b5e354f940722dad07f16d2bad168625
mohamedalmograby <mohamedalmograby@gmail.com>
2020-08-04 08:53:28 -0700

    'removed-all-gitignore'

---
+++ storage/framework/views/2408b4eadafec0c7ba1227bcd8e0cf7ff1e5372f.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href=" <?php echo e(mix('css/app.css')); ?>" rel="stylesheet">
+
+    </head>
+    <body>
+        <div id="app">
+            
+        </div>
+        <script src="<?php echo e(mix('js/bootstrap.js')); ?>"></script>
+        <script src="<?php echo e(mix('js/app.js')); ?>"></script>
+    </body>
+</html>
+<?php /**PATH C:\projects\bigStore\resources\views/landing.blade.php ENDPATH**/ ?>
