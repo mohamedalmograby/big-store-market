5812c08d669558b7f33e62c5c208b44131d2d70f
mohamedalmograby <mohamedalmograby@gmail.com>
2020-08-23 19:27:49 +0200

    add new navbar style

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,12 +2,18 @@ require('./bootstrap');
 //var fs = require("fs");
 
 import Vue from 'vue' ; 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import bootstrap from 'bootstrap';
 import VueRouter from 'vue-router'
 import App from '@/resources/views/App' ; 
 import router from '@/resources/router/router'
 Vue.component('App' , App) ; 
   
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 
 var app = new Vue({
    el: '#app',
