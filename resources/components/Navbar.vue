e9510262ba47f0be592c05cb14e68ab5e59149f1
mohamedalmograby <mohamedalmograby@gmail.com>
2020-08-24 18:51:34 +0200

    add the navbar style again

--- resources/components/Navbar.vue
+++ resources/components/Navbar.vue
@@ -1,28 +1,47 @@
 <template>
     <div>
-        <nav  @loggedIn="change" class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
+
+
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">
                 <router-link :to="{name: 'home'}" class="navbar-brand">Big Store</router-link>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <router-link :to="{ name: 'login' }" class="nav-link" v-if="!isLoggedIn">Login</router-link>
-                        <router-link :to="{ name: 'register' }" class="nav-link" v-if="!isLoggedIn">Register</router-link>
-                        <span v-if="isLoggedIn">
-                            <router-link :to="{ name: 'userboard' }" class="nav-link" v-if="user_type == 0"> Hi, {{name}}</router-link>
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+
+            <!-- Right aligned nav items -->
+
+            
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item>
+                    <router-link :to="{ name: 'login' }" class="nav-link" v-if="!isLoggedIn">Login</router-link>
+                </b-nav-item>
+
+                <b-nav-item>
+                    <router-link :to="{ name: 'register' }" class="nav-link" v-if="!isLoggedIn">Register</router-link>
+                </b-nav-item>
+
+                <b-nav-item-dropdown v-if="isLoggedIn" right>
+                    <!-- Using 'button-content' slot -->
+                    <template v-slot:button-content>
+                        <em>User</em>
+                    </template>
+                    <b-dropdown-item>
+                        <span >
+                            <router-link :to="{ name: 'userboard' }"  v-if="user_type == 0"> Hi, {{name}}</router-link>
                             <router-link :to="{ name: 'admin' }" class="nav-link" v-if="user_type == 1"> Hi, {{name}}</router-link>
                         </span>
-                        <li class="nav-link" v-if="isLoggedIn" @click="logout"> Logout</li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                    </b-dropdown-item>
 
+                    <b-dropdown-item >
+                        <li @click="logout"> Logout</li>
+                    </b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
     </div>
 </template>
 
@@ -60,7 +79,7 @@ export default {
             console.log('here3')
            this.$refs.Navbar.change();
         }
-    }
+    } 
 }
 </script>
 
